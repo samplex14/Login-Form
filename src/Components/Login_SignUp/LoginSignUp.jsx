ec69666813231036bb2093d211d05e2b3d42c8ae
samplex14 <sharmasameep94@gmail.com>
2026-03-01 23:41:42 +0530

    initial commit

--- src/Components/Login_SignUp/LoginSignUp.jsx
+++ src/Components/Login_SignUp/LoginSignUp.jsx
@@ -10,8 +10,36 @@ const LoginSignUp = () => {
         <div className='text'>
           SignUp
         </div>
-      </div>
+        <div className="underline">
 
+        </div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user_icon} alt="" />
+          <input type="text" placeholder='Name'/>
+        </div>
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" placeholder='Email Id'/>
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" placeholder='Password'/>
+        </div>
+      </div>
+      <div className="forgot-password">
+        Forgot Password? <span>Click Here</span>
+      </div>
+      <div className="submit-container">
+        <div className="submit">
+          Sign Up
+        </div>
+        <div className="submit">
+          Login
+        </div>
+        
+      </div>
     </div>
   )
 };
