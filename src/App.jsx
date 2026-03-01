0c4ab57c79ddf09ac032ea40c8c7114c6028cccd
samplex14 <sharmasameep94@gmail.com>
2026-03-01 23:18:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import LoginSignUp from "./Components/Login_SignUp/LoginSignUp"
+
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <LoginSignUp></LoginSignUp>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Login_SignUp/LoginSignUp.jsx
@@ -0,0 +1,19 @@
+import '../../LoginSignUp.css'
+import user_icon from'../../assets/person.png'
+import email_icon from'../../assets/email.png'
+import password_icon from'../../assets/password.png'
+
+const LoginSignUp = () => {
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='text'>
+          SignUp
+        </div>
+      </div>
+
+    </div>
+  )
+};
+
+export default LoginSignUp;
